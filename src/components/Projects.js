edbeafb490856c1fb2ca9bcf0c5b8ef2f3054286
JJeCho <james.je.cho@gmail.com>
2024-03-16 20:50:27 -0700

    df

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,104 +2,63 @@ import React from "react";
 import "./Projects.css";
 
 const Projects = () => {
+  const projects = [
+    {
+      title: "Wordle+",
+      description: "A Full Stack Wordle game built using the MERN Stack. Supports words of different lengths and themes.",
+      imageUrl: "https://source.unsplash.com/random/?wordle",
+      link: "",
+    },
+    {
+      title: "Day of Calendar",
+      description: "A React application that utilizes various 3rd Party API's to collect and display information about holidays, historical events, and more about any given day.",
+      imageUrl: "https://source.unsplash.com/random/?calendar",
+      link: "",
+    },
+    {
+      title: "50 Projects in 50 Days",
+      description: "A collection of 50 projects in HTML, CSS, and JS that I completed to sharpen my web development skills. Completed following along with Brad Traversey's 50 Projects in 50 Days.",
+      imageUrl: "https://source.unsplash.com/random/?html,css,js",
+      link: "",
+    },
+  ];
+
   return (
     <div className="container">
       <h1 className="container-title">Projects</h1>
       <div className="projects">
-        <div className="project">
-          <div className="project-info">
-            <h3 className="project-title">Wordle+</h3>
-            <p className="project-description">
-              A Full Stack Wordle game built using the MERN Stack. Supports
-              words of different lengths and themes.
-              <br />
+        {projects.map((project, index) => (
+          <div key={index} className="project">
+            <div className="project-info">
+              <h3 className="project-title">{project.title}</h3>
+              <p className="project-description">
+                {project.description}
+                <br />
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="project-link"
+                >
+                  View Project
+                </a>
+              </p>
+            </div>
+            <div className="project-image">
               <a
-                href=""
+                href={project.link}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="project-link"
               >
-                View Project
+                <img
+                  src={project.imageUrl}
+                  alt="Random Image"
+                />
               </a>
-            </p>
+            </div>
           </div>
-          <div className="project-image">
-            <a
-              href=""
-              target="_blank"
-              rel="noopener noreferrer"
-              className="project-link"
-            >
-              <img
-                src="https://source.unsplash.com/random/?wordle"
-                alt="Random Image"
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className="project">
-          <div className="project-info">
-            <h3 className="project-title">Day of Calendar</h3>
-            <p className="project-description">
-              A React application that utilizes various 3rd Party API's to collect and display information about holidays, historical events, and more about any given day.
-              <br />
-              <a
-                href=""
-                target="_blank"
-                rel="noopener noreferrer"
-                className="project-link"
-              >
-                View Project
-              </a>
-            </p>
-          </div>
-          <div className="project-image">
-            <a
-              href=""
-              target="_blank"
-              rel="noopener noreferrer"
-              className="project-link"
-            >
-              <img
-                src="https://source.unsplash.com/random/?calendar"
-                alt="Random Image"
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className="project">
-          <div className="project-info">
-            <h3 className="project-title">50 Projects in 50 Days</h3>
-            <p className="project-description">
-              A collection of 50 projects in HTML, CSS, and JS that I completed to sharpen my web development skills.
-              Completed following along with Brad Traversey's 50 Projects in 50 Days.
-              <br />
-              <a
-                href=""
-                target="_blank"
-                rel="noopener noreferrer"
-                className="project-link"
-              >
-                View Project
-              </a>
-            </p>
-          </div>
-          <div className="project-image">
-            <a
-              href=""
-              target="_blank"
-              rel="noopener noreferrer"
-              className="project-link"
-            >
-              <img
-                src="https://source.unsplash.com/random/?html,css,js"
-                alt="Random Image"
-              />
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,9 +3,68 @@ import React from 'react';
 const Resume = () => {
   return (
     <div>
-      <h2>My Resume</h2>
+      <h2>Education</h2>
+      <RenderDegree
+        school="California State University, Long Beach"
+        date="Dec 2023"
+        degree="Bachelor of Science in Computer Science"
+        location="Long Beach, California"
+      />
+      <RenderDegree
+        school="Cerritos College"
+        date="June 2020"
+        degree="Associate in Science for Transfer in Computer Science"
+        location="Norwalk, California"
+      />
+      <RenderDegree
+        school="Gretchen Whitney High School"
+        date="June 2014"
+        degree="High School Diploma"
+        location="Cerritos, California"
+      />
+      <h2>Experience</h2>
+      <RenderExperience
+        company="Traveland"
+        date="March 2022 – Present"
+        location="Cerritos, California"
+        tasks={[
+          'Managed and maintained the company\'s website, ensuring a seamless online shopping experience',
+          'Oversaw e-commerce operations, including order processing, inventory management, and shipping logistics',
+          'Executed business administration tasks, contributing to the overall efficiency of the company'
+        ]}
+      />
+      <RenderExperience
+        company="Uniqlo"
+        date="March 2015 - March 2022"
+        location="Cerritos, California"
+        tasks={[
+          'Led a team of 30+ to surpass performance goals through effective communication, coaching, and cultivating a collaborative work environment',
+          'Conducted comprehensive training sessions on various standards and processes, contributing to the professional development of team members and facilitating effective onboarding for new hires',
+          'Oversaw inventory management processes, ensuring optimal product representation and availability',
+          'Proactively managed customer needs, surpassing expectations based on service standards and principles'
+        ]}
+      />
     </div>
   );
 };
+
+const RenderDegree = ({ school, date, degree, location }) => (
+  <p>
+    <strong>{school}</strong> - {date}<br />
+    {degree}<br />
+    {location}
+  </p>
+);
+
+const RenderExperience = ({ company, date, location, tasks }) => (
+  <div>
+    <h3>{company}</h3>
+    <p><strong>{company}</strong> - {date}<br />{location}</p>
+    <ul>
+      {tasks.map((task, index) => <li key={index}>{task}</li>)}
+    </ul>
+  </div>
+);
+
 
 export default Resume;
